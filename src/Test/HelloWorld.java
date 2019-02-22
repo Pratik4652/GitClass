b6f3694ba21706e4ef3af7771d5e7b923b7a9280
Regan Maharjan <maharjanregan50@gmail.com>
2019-02-22 14:16:21 +0545

    addition using scanner

--- src/Test/HelloWorld.java
+++ src/Test/HelloWorld.java
@@ -1,13 +1,19 @@
 package Test;
-
+import java.util.Scanner;
 public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
-        addition(3,4);
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter a first number: ");
+        int a = reader.nextInt();
+
+        System.out.println("Enter a second number: ");
+        int b = reader.nextInt();
+        addition(a,b);
 
     }
     public static void addition(int a , int b){
-        System.out.println(a+b);
+        System.out.println("Total: " + (a+b));
     }
 }
