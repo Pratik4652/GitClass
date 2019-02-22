d5451ecbb8279e030484b82d3850ff6ad427e071
Regan Maharjan <maharjanregan50@gmail.com>
2019-02-22 14:10:49 +0545

    simple addition function passing variable

--- src/Test/HelloWorld.java
+++ src/Test/HelloWorld.java
@@ -4,5 +4,10 @@ public class HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+        addition(3,4);
+
+    }
+    public static void addition(int a , int b){
+        System.out.println(a+b);
     }
 }
