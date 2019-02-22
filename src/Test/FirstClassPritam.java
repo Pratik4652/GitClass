a680bcfbacdc277eb4b5630f4535eba898d071b4
pritamTron <rider15.ps@gmail.com>
2019-02-22 15:59:09 +0545

    passed variables in function parameter

--- src/Test/FirstClassPritam.java
+++ src/Test/FirstClassPritam.java
@@ -1,7 +1,35 @@
 package Test;
+import java.util.Scanner;
 
 public class FirstClassPritam {
     public static void main(String [] args){
         System.out.println("Hello World!!!");
+
+        SecondClassPritam obj = new SecondClassPritam();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter two numbers for addition:");
+        int n1=sc.nextInt();
+        int n2=sc.nextInt();
+        int sum = obj.addition(n1, n2);
+        System.out.println(sum);
+
+        System.out.println("Enter two numbers for subtraction:");
+        int n3=sc.nextInt();
+        int n4=sc.nextInt();
+        int diff = obj.subtraction(n3,n4);
+        System.out.println(diff);
+
+        System.out.println("Enter two numbers for multiplication:");
+        int n5=sc.nextInt();
+        int n6=sc.nextInt();
+        int mul = obj.multiplication(n5, n6);
+        System.out.println(mul);
+
+        System.out.println("Enter two numbers for division:");
+        float n7=sc.nextFloat();
+        float n8=sc.nextFloat();
+        float div = obj.division(n7,  n8);
+        System.out.println(div);
     }
 }
---
+++ src/Test/SecondClassPritam.java
@@ -0,0 +1,20 @@
+package Test;
+
+public class SecondClassPritam {
+    public int addition(int a, int b){
+        int add = a + b;
+        return add;
+    }
+    public int subtraction(int a, int b){
+        int sub = a - b;
+        return sub;
+    }
+    public int multiplication(int a, int b){
+        int mul = a * b;
+        return mul;
+    }
+    public float division (float a, float b){
+        float divide = a / b;
+        return divide;
+    }
+}
