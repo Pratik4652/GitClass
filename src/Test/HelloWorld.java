b15ee572a9cdeddf1b9b5af79f0f932ca3dc24c3
Regan Maharjan <maharjanregan50@gmail.com>
2019-02-22 14:43:25 +0545

    doing using different class

--- src/Test/HelloWorld.java
+++ src/Test/HelloWorld.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class HelloWorld {
 
     public static void main(String[] args) {
+
         System.out.println("Calculation");
         Scanner reader = new Scanner(System.in);  // Reading from System.in
         System.out.println("Enter a first number: ");
@@ -10,15 +11,18 @@ public class HelloWorld {
 
         System.out.println("Enter a second number: ");
         float b = reader.nextFloat();
-        addition(a,b);
+
+        calculator obj = new calculator();
+        System.out.println( "When added" + obj.addition(a,b));
+       // calculator(a,b);
         substraction(a,b);
         multiplication(a,b);
         division(a,b);
 
     }
-    public static void addition(float a , float b){
+  /*  public static void calculator(float a , float b){
         System.out.println("When added: " + (a+b));
-    }
+    }*/
 
     public static void substraction(float a , float b){
         System.out.println("when substracted: " + (a-b));
---
+++ src/Test/calculator.java
@@ -0,0 +1,10 @@
+package Test;
+
+public class calculator {
+
+    public float addition(float a, float b){
+        float sum;
+        sum = a + b;
+        return sum;
+    }
+}
