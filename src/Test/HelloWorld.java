2ff24200a25293c01765f7ac1eee63f4adcd3f00
Anushka Shikhrakar <anushka.shikhrakar@ekbana.info>
2019-02-22 16:27:02 +0545

    The first assignment

--- src/Test/HelloWorld.java
+++ src/Test/HelloWorld.java
@@ -1,7 +1,13 @@
 package Test;
 
 public class HelloWorld {
-
+    class Add
+    {
+        int result=0;
+        Add(int x, int y){
+            result=x+y;
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
--- src/Test/PrintAnushka.java
+++
@@ -1,7 +0,0 @@
-package Test;
-
-public class PrintAnushka {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
