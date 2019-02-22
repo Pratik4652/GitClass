7c603d93b5a8185b2c687b499d6420819d94e9e0
Regan Maharjan <maharjanregan50@gmail.com>
2019-02-22 14:26:28 +0545

    adding,substracting, multiplying and dividing

--- src/Test/HelloWorld.java
+++ src/Test/HelloWorld.java
@@ -3,17 +3,30 @@ import java.util.Scanner;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Calculation");
         Scanner reader = new Scanner(System.in);  // Reading from System.in
         System.out.println("Enter a first number: ");
-        int a = reader.nextInt();
+        float a = reader.nextFloat();
 
         System.out.println("Enter a second number: ");
-        int b = reader.nextInt();
+        float b = reader.nextFloat();
         addition(a,b);
+        substraction(a,b);
+        multiplication(a,b);
+        division(a,b);
 
     }
-    public static void addition(int a , int b){
-        System.out.println("Total: " + (a+b));
+    public static void addition(float a , float b){
+        System.out.println("When added: " + (a+b));
+    }
+
+    public static void substraction(float a , float b){
+        System.out.println("when substracted: " + (a-b));
+    }
+    public static void multiplication(float a , float b){
+        System.out.println("when multiply: " + (a*b));
+    }
+    public static void division(float a , float b){
+        System.out.println("when divide: " + (a/b));
     }
 }
